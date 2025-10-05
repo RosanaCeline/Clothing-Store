7e13fd96ddaf10b921fd63c393fddfc13c049eea
Rosana Celine <celine.rosana@gmail.com>
2025-10-05 18:25:01 -0300

    ci: changing to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes/routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         {routes.map(({ path, element, children }) => (
           <Route key={path} path={path} element={element}>
@@ -14,15 +14,19 @@ function App() {
                 child.index ? (
                   <Route key={index} index element={child.element} />
                 ) : (
-                  <Route key={child.path} path={child.path} element={child.element} />
+                  <Route
+                    key={child.path}
+                    path={child.path}
+                    element={child.element}
+                  />
                 )
               )}
           </Route>
         ))}
       </Routes>
 
-      <ToastContainer position="top-right" autoClose={3000} style={{ top: "105px" }} />
-    </HashRouter>
+      <ToastContainer position="top-right" autoClose={3000} style={{ top: "15px" }} />
+    </BrowserRouter>
   );
 }
 
